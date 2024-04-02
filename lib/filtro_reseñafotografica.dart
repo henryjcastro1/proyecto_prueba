9579b146e4adb94a9bf90bf6326399194f448420
Henry Castro <63067620+xlelgatolx@users.noreply.github.com>
2024-04-01 21:58:50 -0500

    Se agrego registro de reseñas

---
+++ lib/filtro_reseñafotografica.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResenaFotografica extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reseña Fotográfica'),
+      ),
+      body: Center(
+        child: Text('Contenido de la reseña fotográfica'),
+      ),
+    );
+  }
+}
